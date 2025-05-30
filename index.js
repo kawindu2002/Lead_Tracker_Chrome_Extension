d15f5bbd23218eae477d258c0a4871637f6c831f
kawindu2002 <kawindupriyashan@gmail.com>
2025-05-30 22:43:25 +0530

    add files

---
+++ index.js
@@ -0,0 +1,48 @@
+
+let inputBtn = document.querySelector("#input-btn");
+const inputEl = document.querySelector("#input-el")
+const ulEl = document.getElementById("ul-el")
+
+let myLeads = []
+
+// localStorage.clear();
+
+let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+if (leadsFromLocalStorage) {
+        myLeads = leadsFromLocalStorage;
+        renderLeads();
+}
+
+
+inputBtn.addEventListener("click",function(){
+        if (inputEl.value == null || inputEl.value === ""){
+                alert("Please enter link");
+        }else{
+                myLeads.push(inputEl.value);
+                inputEl.value = "";
+                
+                localStorage.setItem("myLeads", JSON.stringify(myLeads));
+                renderLeads();
+                
+                console.log(localStorage.getItem("myLeads"));
+        }
+
+});
+
+function renderLeads(){
+        let listItems = "";
+        
+        for (let i = 0; i < myLeads.length; i++) {
+                listItems +=  `
+                                    <li>
+                                        <a target='_blank' href='${myLeads[i]}'>
+                                            ${myLeads[i]}
+                                        </a>
+                                    </li>
+                                `
+                
+        }
+        
+        ulEl.innerHTML = listItems;
+        
+}
